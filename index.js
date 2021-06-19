32db2ee6a725a67a22c70bddfc0cfbf9a0dc06ab
engamir1 <83735964+engamir1@users.noreply.github.com>
2021-06-20 00:04:20 +0200

    add bounce function

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const debounce = (func) => {
     }
     timeoutid = setTimeout(() => {
       //       func.apply(null, args);  .apply () take all args and add to func as seperate arguments
-
       func.apply(null, args);
     }, 1000);
   };
