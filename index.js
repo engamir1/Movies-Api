6f3039380788bd8bd00d08ad96c21e06c7ceea9e
engamir1 <83735964+engamir1@users.noreply.github.com>
2021-06-20 00:02:53 +0200

    add bounce function

--- index.js
+++ index.js
@@ -10,22 +10,35 @@ const getData = async (eventData) => {
   console.log(response.data);
 };
 // = null
-let timeoutid;    
 
+const debounce = (func) => {
+  let timeoutid;
+  return (...args) => {
+    if (timeoutid) {
+      clearTimeout(timeoutid);
+    }
+    timeoutid = setTimeout(() => {
+      //       func.apply(null, args);  .apply () take all args and add to func as seperate arguments
+
+      func.apply(null, args);
+    }, 1000);
+  };
+};
+
+let timeoutid;
 const oninput = (event) => {
-// to prevent setTimeout from work
-// every seTimeout has time identifire  1 - 2 - 3 ....
-// if u want to cancel setimeout before execut function 
+  // to prevent setTimeout from work
+  // every seTimeout has time identifire  1 - 2 - 3 ....
+  // if u want to cancel setimeout before execut function
   if (timeoutid) {
     clearTimeout(timeoutid);
   }
   timeoutid = setTimeout(() => {
-      
     getData(event.target.value);
   }, 500);
 };
 
 const input = document.querySelector("input");
-input.addEventListener("input", oninput);
+input.addEventListener("input", debounce(oninput));
 
 getData(input);
